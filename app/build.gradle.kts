a99c04ac59e7e3b6eca4e0c29cd79829dace3ce6
simpolism <simpolism@gmail.com>
2026-09-05 15:26:10 -0400

    Fix interleaved Genesis DAC playback

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "org.vlessert.vgmp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.7.2"
+        versionCode = 18
+        versionName = "1.7.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
